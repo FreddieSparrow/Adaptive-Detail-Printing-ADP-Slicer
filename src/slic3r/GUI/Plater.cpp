b9973086c9f2699bbee2f8212c7a1d6ab749de82
Enrico Gette <en.gette@sprinteins.com>
2026-03-26 15:11:41 +0100

    tidy up code

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -62,7 +62,6 @@
 #include "libslic3r/Polygon.hpp"
 #include "libslic3r/Print.hpp"
 #include "libslic3r/PrintConfig.hpp"
-#include "libslic3r/PresetSwitchLogic.hpp"
 #include "libslic3r/SLAPrint.hpp"
 #include "libslic3r/Utils.hpp"
 #include "libslic3r/PresetBundle.hpp"
@@ -81,6 +80,7 @@
 #endif
 #include "GUI_Utils.hpp"
 #include "GUI_Factories.hpp"
+#include "PresetSwitchLogic.hpp"
 #include "wxExtensions.hpp"
 #include "MainFrame.hpp"
 #include "format.hpp"
@@ -9334,14 +9334,14 @@ void Plater::priv::on_select_preset(wxCommandEvent &evt)
         combo->ShowBadge(select_flag == (int)PresetComboBox::FilamentAMSType::FROM_AMS);
         q->on_filament_change(idx);
     }
-    bool select_preset = !combo->selection_is_changed_according_to_physical_printers();
-    // TODO: ?
-    if (preset_type == Preset::TYPE_FILAMENT && sidebar->is_multifilament()) {
+    const bool select_preset = !combo->selection_is_changed_according_to_physical_printers();
+    switch (preset_selection_followup(preset_type, select_preset, sidebar->is_multifilament())) {
+    case PresetSelectionFollowup::UpdateComboOnly:
         // Only update the plater UI for the 2nd and other filaments.
         combo->update();
-    }
-    else if (select_preset) {
-        if (preset_type == Preset::TYPE_PRINTER) {
+        break;
+
+    case PresetSelectionFollowup::RunPrinterSelectionFlow: {
             PhysicalPrinterCollection& physical_printers = wxGetApp().preset_bundle->physical_printers;
             if(combo->is_selected_physical_printer())
                 preset_name = physical_printers.get_selected_printer_preset_name();
@@ -9394,13 +9394,19 @@ void Plater::priv::on_select_preset(wxCommandEvent &evt)
                     }
                 }
             }
+            break;
         }
-        if (should_run_generic_preset_selection(preset_type, select_preset, sidebar->is_multifilament())) {
-            //BBS
-            //wxWindowUpdateLocker noUpdates1(sidebar->print_panel());
-            wxWindowUpdateLocker noUpdates2(sidebar->filament_panel());
-            wxGetApp().get_tab(preset_type)->select_preset(preset_name);
-        }
+
+    case PresetSelectionFollowup::RunGenericTabSelection: {
+        //BBS
+        //wxWindowUpdateLocker noUpdates1(sidebar->print_panel());
+        wxWindowUpdateLocker noUpdates2(sidebar->filament_panel());
+        wxGetApp().get_tab(preset_type)->select_preset(preset_name);
+        break;
+    }
+
+    case PresetSelectionFollowup::None:
+        break;
     }
 
     // update plater with new config
